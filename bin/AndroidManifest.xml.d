d7fd725d3cfeb4da0cb3dfe8e4cccaa5e597a66e
Chris Lane <chris@chris-allen-lane.com>
2012-11-05 18:16:28 -0500

    Committing a clean new project.

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/chris/Source/phonegap/presentation-timer-pro/bin/AndroidManifest.xml : \
+/home/chris/Source/phonegap/presentation-timer-pro/AndroidManifest.xml \
---
+++ gen/R.java.d
@@ -0,0 +1,14 @@
+/home/chris/Source/phonegap/presentation-timer-pro/gen/com/chrisallenlane/presentationtimerpro/R.java \
+ : /home/chris/Source/phonegap/presentation-timer-pro/res/drawable-hdpi/ic_launcher.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable-hdpi/icon.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable/icon.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/values/strings.xml \
+/home/chris/Source/phonegap/presentation-timer-pro/res/xml/config.xml \
+/home/chris/Source/phonegap/presentation-timer-pro/res/layout/main.xml \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable-xhdpi/ic_launcher.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable-xhdpi/icon.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable-ldpi/ic_launcher.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable-ldpi/icon.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable-mdpi/ic_launcher.png \
+/home/chris/Source/phonegap/presentation-timer-pro/res/drawable-mdpi/icon.png \
+/home/chris/Source/phonegap/presentation-timer-pro/bin/AndroidManifest.xml \

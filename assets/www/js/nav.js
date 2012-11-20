9528c082345261eed095fb4b19538c9e035cbbf5
Chris Lane <chris@chris-allen-lane.com>
2012-11-20 16:49:57 -0500

    Trying to fix some random Phonegap explosion.

--- assets/www/js/nav.js
+++ assets/www/js/nav.js
@@ -4,8 +4,25 @@ nav = {
     doBackButton: function(){
         // from timer: exit application
         if($.mobile.activePage.is('#page-timer')){ navigator.app.exitApp(); }
+
         // from menu: return to timer
         else if($.mobile.activePage.is('#page-menu')){ window.location = '#page-timer'; }
+
+        // from settings page
+        else if($.mobile.activePage.is('#page-settings')){
+            // hide the color picker if it is being displayed
+            if($('div.simpleColorChooser').is(':visible')){
+                // I've inspected the color chooser plugin's code, and the cancel
+                // button seems to do little more than this, fortunately
+                $('div.simpleColorChooser').hide();
+            }
+            
+            // otherwise, return to the menu page
+            else {
+                window.location = '#page-menu';
+            }
+        }
+
         // from other pages: return to menu
         else { window.location = '#page-menu'; }
     },
